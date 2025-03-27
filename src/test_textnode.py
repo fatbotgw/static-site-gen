ed5f72c8054eeb9c9d4329d06e1fbe7f954c97f1
Greg W <1514406+fatbotgw@users.noreply.github.com>
2025-03-27 17:15:02 -0400

    Added more tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,26 +4,42 @@ from textnode import TextNode, TextType
 
 
 class TestTextNode(unittest.TestCase):
+    # Text tests
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
-    def url_none(self):
-        node = TextNode("test node", TextType.LINK)
-        node2 = TextNode("test node", TextType.LINK, None)
-        self.assertEqual(node, node2)
+    def test_diff_text(self):
+        node = TextNode("different text", TextType.IMAGE, "https://boot.dev")
+        node2 = TextNode("test node", TextType.IMAGE, "https://boot.dev")
+        self.assertNotEqual(node, node2)
 
-    def diff_type(self):
+    # TextType tests
+    def test_diff_type(self):
         node = TextNode("test node", TextType.BOLD)
-        node2 = TextNode("test node", TextType.LINK, None)
+        node2 = TextNode("test node", TextType.PLAIN)
         self.assertNotEqual(node, node2)
 
-    def diff_type2(self):
+    # URL tests
+    def test_link_without_url_raises_exception(self):
+        with self.assertRaises(Exception):
+            node = TextNode("test node", TextType.LINK)
+
+    def test_url_none(self):
+        node = TextNode("test node", TextType.PLAIN)
+        node2 = TextNode("test node", TextType.PLAIN, "https://boot.dev")
+        self.assertNotEqual(node, node2)
+
+    def test_diff_url(self):
         node = TextNode("test node", TextType.IMAGE, "https://boot.dev")
-        node2 = TextNode("test node", TextType.IMAGE, None)
+        node2 = TextNode("test node", TextType.IMAGE, "https://www.google.com")
         self.assertNotEqual(node, node2)
 
+    def test_url_eq(self):
+        node = TextNode("text", TextType.IMAGE, "https://boot.dev")
+        node2 = TextNode("text", TextType.IMAGE, "https://boot.dev")
+        self.assertEqual(node, node2)
 
 
 
