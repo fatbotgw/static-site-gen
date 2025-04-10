2a892dddde47477fafe5c2abe3874fc0f6975085
Greg W <1514406+fatbotgw@users.noreply.github.com>
2025-04-09 23:45:00 -0400

    Moved copy_static_to_public() from main.py to generate.py, laid groundwork for upcoming generate_page() function

---
+++ src/generate.py
@@ -0,0 +1,37 @@
+from shutil import copy, rmtree
+from os import mkdir, listdir
+from os.path import isfile, isdir, join, abspath
+
+def copy_static_to_public(source=None, destination=None):
+    if source is None:
+        source = abspath("static")
+        destination = abspath("public")
+
+    # delete everything in public
+        print("deleting public/")
+        rmtree("public/", ignore_errors=True)
+        print("creating public/")
+        mkdir("public")
+
+    # copy everything from static to public
+    dir_tree = listdir(source)
+    for item in dir_tree:
+        if item == ".DS_Store":
+            continue
+        if not isfile(join(source, item)):
+            mkdir(join(destination, item))
+            copy_static_to_public(join(source, item), join(destination, item))
+
+        if isfile(join(source, item)):
+            copy(join(source, item), join(destination, item))
+
+    return
+
+
+def extract_title(markdown):
+    pass
+
+
+
+def generate_page(from_path, template_path, dest_path):
+    pass
--- src/main.py
+++ src/main.py
@@ -1,36 +1,8 @@
-from shutil import copy, rmtree
-from os import mkdir, listdir
-from os.path import isfile, isdir, join, abspath
-
-
-def copy_static_to_public(source=None, destination=None):
-    if source is None:
-        source = abspath("static")
-        destination = abspath("public")
-
-    # delete everything in public
-        print("deleting public/")
-        rmtree("public/", ignore_errors=True)
-        print("creating public/")
-        mkdir("public")
-
-    # copy everything from static to public
-    dir_tree = listdir(source)
-    for item in dir_tree:
-        if item == ".DS_Store":
-            continue
-        if not isfile(join(source, item)):
-            mkdir(join(destination, item))
-            copy_static_to_public(join(source, item), join(destination, item))
-
-        if isfile(join(source, item)):
-            copy(join(source, item), join(destination, item))
-
-    return
-
+from generate import copy_static_to_public, generate_page
 
 def main():
     copy_static_to_public()
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 if __name__ == "__main__":
     main()
