dfc5eed0d43c2a6f8b7efa6d214eae2b7a7aa225
Greg W <1514406+fatbotgw@users.noreply.github.com>
2025-04-10 21:11:10 -0400

    Completed generate_page()

--- src/generate.py
+++ src/generate.py
@@ -1,6 +1,8 @@
 from shutil import copy, rmtree
-from os import mkdir, listdir
-from os.path import isfile, isdir, join, abspath
+from os import makedirs, mkdir, listdir
+from os.path import dirname, isfile, isdir, join, abspath, exists
+from converters import markdown_to_html_node
+from htmlnode import ParentNode
 
 def copy_static_to_public(source=None, destination=None):
     if source is None:
@@ -38,3 +40,25 @@ def extract_title(markdown):
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    src_doc = ""    # This is the markdown text document
+    template = ""   # This is the html template document
+
+    with open(from_path) as file:
+        src_doc = file.read()
+        file.close()
+    with open(template_path) as file:
+        template = file.read()
+        file.close()
+
+    html_out = markdown_to_html_node(src_doc)
+    title = extract_title(src_doc)
+
+    out_doc = template.replace("{{ Title }}", title).replace("{{ Content }}", html_out.to_html())
+        
+    if not exists(dirname(dest_path)):
+        print(f"'{dirname(dest_path)}' does not exist...creating path...")
+        makedirs(dirname(dest_path))
+    with open(dest_path, mode='w') as file:
+        file.write(out_doc)
+        file.close()
